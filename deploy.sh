e3762b29c75de19a7111a87dcb6b2bccbda49af7
Cole Roof <ckroof@vt.edu>
2022-01-26 19:07:07 -0500

    Moved to deploying to Raspberry Pi - configured things for cross compilation to the Raspberry Pi (3b+) - created a deploy script to build, deploy, and run on target - added openssl to player's Cargo.toml because compilation broke without it (when cross compiling) - built a SUPER SIMPLE webserver using the Rocket crate

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly TARGET_HOST=janus@janus
+readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
+
+readonly TARGET_PLAYER_PATH=/home/janus/rust/record-player/player
+readonly TARGET_WEBSERVER_PATH=/home/janus/rust/record-player/webserver
+
+readonly LOCAL_PLAYER_PATH=./target/${TARGET_ARCH}/release/player
+readonly LOCAL_WEBSERVER_PATH=./target/${TARGET_ARCH}/release/webserver
+
+# Build the executables
+cargo build --release --target=${TARGET_ARCH}
+
+# Send the executables to the rpi
+rsync ${LOCAL_PLAYER_PATH} ${TARGET_HOST}:${TARGET_PLAYER_PATH}
+rsync ${LOCAL_WEBSERVER_PATH} ${TARGET_HOST}:${TARGET_WEBSERVER_PATH}
+
+# Run the webserver
+ssh -t ${TARGET_HOST} ${TARGET_WEBSERVER_PATH}
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, from webserver");
+#[macro_use] extern crate rocket;
+
+use rocket::response::content::Html;
+
+#[get("/")]
+fn index() -> Html<&'static str> {
+    Html(
+    r#"
+    <html>
+        <body> 
+            Hello from Raspberry Pi
+            <a href="http://youtube.com">
+                <button>Click Me</button>
+            </a>
+        </body>
+    </html>
+    "#
+    )
+}
+
+#[launch]
+fn rocket() -> _ {
+
+    let config = rocket::Config::figment()
+                    .merge(("port", 8080))
+                    .merge(("address", "0.0.0.0"));
+
+    rocket::custom(config)
+                    .mount("/", routes![index])
 }
