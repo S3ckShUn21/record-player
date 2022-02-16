00d0aa921e1f31cbf2827f6b20607951be484655
Cole Roof <ckroof@vt.edu>
2022-02-16 02:51:16 -0500

    added some interactivity using hooks

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,10 +1,23 @@
 use yew::prelude::*;
 
-// use yew::{function_component, html};
-
 #[function_component(HelloWorld)]
 fn hello_world() -> Html {
-    html! { "This Text" }
+
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "Increment value" }</button>
+            <p>
+                <b>{ "Current value: " }</b>
+                { *counter }
+            </p>
+        </div>
+    }
 }
 
 fn main() {
